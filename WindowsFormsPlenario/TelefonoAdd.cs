719504b6f80cb833efe515e767a1e9994675de7c
Yamil Foglia <yamil.foglia@outlook.com>
2021-03-09 16:48:10 -0300

    Agregar archivos de proyecto.

---
+++ WindowsFormsPlenario/TelefonoAdd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsPlenario
+{
+    public partial class TelefonoAdd : Form
+    {
+        public TelefonoAdd()
+        {
+            InitializeComponent();
+        }
+    }
+}
